ce644552d1d09641c4951e9105bc95deee704b97
Ewaboocee <odujinrinbusola02@gmail.com>
2023-08-16 08:40:24 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#includw "main"
+/**
+  * _islower - shows 1 if the input is a
+  * lowercase character. Another cases, shows
+  * 0 
+  *
+  * Qc: The character in ASCII code
+  *
+  * Return: 1 for lowercase character. 0 fot the rest.
+  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
